db1afac8ab89fac3b6e327139015dd451384e6dc
Subrata Ghosh <subrata.ghosh.ece@gmail.com>
2024-10-26 09:10:13 +0530

    updated sitemap.xml file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,8 +6,6 @@ module.exports = {
     siteUrl: 'https://www.subrataghosh.in', // Replace with your website's URL
     generateRobotsTxt: true,            // Generates a robots.txt file
     sitemapSize: 5000,                   // Limits the maximum number of URLs per sitemap file
-    changefreq: 'weekly',                // Update frequency (e.g., daily, weekly, monthly)
-    priority: 0.7,                       // Default priority for all URLs
     exclude: ['/admin/*'],               // Exclude specific routes if needed
     robotsTxtOptions: {
       policies: [
